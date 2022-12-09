cfab6d10d9aa8a0b08120c8e71282bf2a30f164f
Azad117 <95510274+Azad117@users.noreply.github.com>
2022-12-09 20:00:48 +0530

    Add files via upload

---
+++ Binary Search With Recurrsion.C.C
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int binary(int a[20],int low,int high,int key);
+void main()
+{
+	int i,n,key,a[20];
+	clrscr();
+	printf("Enter no of elements\n");
+	scanf("%d",&n);
+	printf("Enter your values:\n");
+	for(i=0;i<n;i++)
+		scanf("%d",&a[i]);
+	printf("Enter the Key Value:\n");
+	scanf("%d",&key);
+	binary(a,0,n-1,key);
+	getch();
+}
+int binary(int a[20],int low,int high,int key)
+{
+	int mid,found;
+	while(low<=high)
+	{
+		mid=(low+high)/2;
+		if(a[mid]==key)
+		{
+			found=1;
+			printf("The element is found in the index %d\n",mid);
+			break;
+		}
+		else if(a[mid]<key)
+		{
+			return binary(a,mid+1,high,key);
+		}
+		else
+			return binary(a,low,mid-1,key);
+	}
+	if(found==1)
+		printf("the element is found\n");
+}
+
